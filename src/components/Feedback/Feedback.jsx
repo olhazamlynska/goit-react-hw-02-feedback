6c0ef393f373eb0e494887916e7fea80de44a22a
olhazamlynska <olya.sonechko@gmail.com>
2022-11-30 14:54:34 +0200

    Add step 1

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGood = e => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  handleNeutral = e => {
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  };
+  handleBad = e => {
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <>
+          <button type="button" onClick={this.handleGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.handleNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleBad}>
+            Bad
+          </button>
+        </>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+      </div>
+    );
+  }
+}
